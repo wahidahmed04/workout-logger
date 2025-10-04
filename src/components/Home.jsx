401b602dca915338a19ec337326a0d0efb00758c
Wahid Ahmed <wahidahmed@Mac.lan>
2025-10-04 13:19:29 -0400

    added basic ui to homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,27 +3,44 @@ import { supabase } from '../supabaseClient'
 import {useState, useEffect} from 'react'
 export default function Home({session}) {
   const [username, setUsername] = useState('')
-    useEffect(() => {
-    if (!session?.user) return
+  const [accountAge, setAccountAge] = useState(null)
+  useEffect(() => {
+  if (!session?.user) return
 
-    async function fetchProfile() {
-      const { data, error } = await supabase
-        .from('profiles')
-        .select('username')
-        .eq('id', session.user.id)
-        .single()
+  async function fetchProfile() {
+    const { data, error } = await supabase
+      .from('profiles')
+      .select('username, created_at')
+      .eq('id', session.user.id)
+      .single()
 
-      if (error) console.error('Error fetching profile:', error)
-      else setUsername(data.username)
+    if (error) console.error('Error fetching profile:', error)
+    else {
+      setUsername(data.username)
+      const createdAt = new Date(data.created_at);
+    const now = new Date();
+    const diffMs = now - createdAt; // difference in milliseconds
+    const diffDays = Math.floor(diffMs / (1000 * 60 * 60 * 24)); // convert ms to days
+
+    setAccountAge(diffDays);
     }
+  }
 
-    fetchProfile()
-  }, [session])
+  fetchProfile()
+}, [session])
+
   return (
     <>
     <h1>Homepage</h1>
     <Link to="/">Go back</Link>
     <h1>Username: {username}</h1>
+    <h1>Account age: {accountAge} {accountAge === 1 ? "day" : "days"}</h1>
+    <h1>Favorite exercises: </h1>
+    <h1>Total number of workouts:</h1>
+    <br/>
+    <Link to="/logger">Log a workout</Link>
+    <br/>
+    <Link to="/history">View your workout history</Link>
     </>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: '/workout-logger/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  },
+  base: '/workout-logger/',
 })
