46bc0bd9548520d1f5548f09cef7c1def6ad312e
Wahid Ahmed <wahidahmed@Mac.lan>
2025-10-28 18:34:29 -0400

    Attempt to fix routing issue

--- src/support-components/SignIn.jsx
+++ src/support-components/SignIn.jsx
@@ -25,7 +25,7 @@ export default function SignIn({ session, setSession, setUserSigningIn }) {
     const { error } = await supabase.auth.signInWithOAuth({
   provider: 'google',
   options: {
-    redirectTo: window.location.origin, // works for localhost & GitHub Pages
+    redirectTo,// works for localhost & GitHub Pages
   },
 });
     if (error) console.error(error.message)
